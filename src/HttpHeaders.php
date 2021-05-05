06680a956b04aebde8e8ae89f89aa4cea08b685b
Florent Hernandez <florent.hernandez@greenpeace.org>
2021-05-05 14:49:39 +0200

    PLANET-6115: CSP directives hook

--- src/HttpHeaders.php
+++ src/HttpHeaders.php
@@ -13,38 +13,46 @@ class HttpHeaders {
 		add_action( 'wp_headers', [ $this, 'send_content_security_policy_header' ], 10, 1 );
 	}
 
-
 	/**
 	 * Send Content Security Policy (CSP) HTTP headers.
 	 *
 	 * @param string[] $headers Associative array of headers to be sent.
 	 */
 	public function send_content_security_policy_header( $headers ): array {
-		$default_allowed_frame_ancestors = [ '\'self\'' ];
-
-		/**
-		 * Filter hook to add trusted frame ancestors to the Content Security Policy.
-		 *
-		 * @param array $additional_allowed_frame_ancestors Array of domains to whitelist as frame ancestors.
-		 */
-		$additional_allowed_frame_ancestors = apply_filters( 'planet4_csp_allowed_frame_ancestors', [] );
-
-		if ( is_array( $additional_allowed_frame_ancestors ) ) {
-			$allowed_frame_ancestors = array_merge( $default_allowed_frame_ancestors, $additional_allowed_frame_ancestors );
-		}
-
-		$directives = [
-			'default-src * \'self\' data: \'unsafe-inline\' \'unsafe-eval\'',
-			'frame-ancestors ' . implode( ' ', $allowed_frame_ancestors ),
+		$default_directives = [
+			'default-src'     => [
+				'*',
+				'\'self\'',
+				'data:',
+				'\'unsafe-inline\'',
+				'\'unsafe-eval\'',
+			],
+			'frame-ancestors' => [ '\'self\'' ],
 		];
 
-		$csp_header = implode( '; ', $directives );
-		$csp_header = preg_replace( "/\r|\n/", '', $csp_header );
+		/**
+		 * Hook usage:
+		 * add_filter( 'planet4_csp_directives', function ( $directives ) {
+		 *     $directives['img-src'] = [ '*', 'blob:', 'data:' ];
+		 *     $directives['frame-ancestors'][] = 'https://www.example.org';
+		 *     return $directives;
+		 * } );
+		 */
+		$directives = apply_filters( 'planet4_csp_directives', $default_directives );
 
+		$csp_header = implode('; ', array_map(function ( $key, $values ) {
+				return $key . ' ' . implode( ' ', array_filter( $values ) );
+			},
+			array_keys( $directives ),
+			$directives
+		));
+		$csp_header = preg_replace( "/\r|\n/", '', $csp_header );
 		$headers['Content-Security-Policy'] = $csp_header;
 
 		// In addition, send the "X-Frame-Options" header when no other trusted frame ancestors were added through the filter.
-		if ( $allowed_frame_ancestors === $default_allowed_frame_ancestors ) {
+		if ( ! empty( $directives['frame-ancestors'] )
+			&& $default_directives['frame-ancestors'] === $directives['frame-ancestors']
+		) {
 			$headers['X-Frame-Options'] = 'SAMEORIGIN';
 		}
 
